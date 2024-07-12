e3f14649c9ac02baa2ebf8609b0238985ffe0600
hunainchhipa <hunain.chhipa23@gmail.com>
2024-07-12 18:53:42 +0530

    added sidebar
    added sidebar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,72 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import WrappedButton from "./GlobalComponents/WrappedButton";
+import {
+  CBadge,
+  CNavGroup,
+  CNavItem,
+  CNavTitle,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarHeader,
+  CSidebarNav,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import {
+  cilCloudDownload,
+  cilLayers,
+  cilPuzzle,
+  cilSpeedometer,
+} from "@coreui/icons";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <>
+      <CSidebar className="border-end vh-100" colorScheme="dark" unfoldable>
+        <CSidebarHeader className="border-bottom">
+          <CSidebarBrand>SWM</CSidebarBrand>
+        </CSidebarHeader>
+        <CSidebarNav>
+          <CNavTitle>Nav Title</CNavTitle>
+          <CNavItem href="#">
+            <CIcon customClassName="nav-icon" icon={cilSpeedometer} /> Nav item
+          </CNavItem>
+          <CNavItem href="#">
+            <CIcon customClassName="nav-icon" icon={cilSpeedometer} /> With
+            badge <CBadge color="primary ms-auto">NEW</CBadge>
+          </CNavItem>
+          <CNavGroup
+            toggler={
+              <>
+                <CIcon customClassName="nav-icon" icon={cilPuzzle} /> Nav
+                dropdown
+              </>
+            }
+          >
+            <CNavItem href="#">
+              <span className="nav-icon">
+                <span className="nav-icon-bullet"></span>
+              </span>{" "}
+              Nav dropdown item
+            </CNavItem>
+            <CNavItem href="#">
+              <span className="nav-icon">
+                <span className="nav-icon-bullet"></span>
+              </span>{" "}
+              Nav dropdown item
+            </CNavItem>
+          </CNavGroup>
+          <CNavItem href="#">
+            <CIcon customClassName="nav-icon" icon={cilCloudDownload} />{" "}
+            Download CoreUI
+          </CNavItem>
+          <CNavItem href="#">
+            <CIcon customClassName="nav-icon" icon={cilLayers} /> Try CoreUI PRO
+          </CNavItem>
+        </CSidebarNav>
+      </CSidebar>
+
       <div className="d-flex justify-content-center align-items-center gap-2 vh-100">
         <WrappedButton
           className="btn btn-outline-secondary"
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@coreui/coreui/dist/css/coreui.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
